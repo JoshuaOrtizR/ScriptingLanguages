3e8be20dad291eca95c45d7b350be8497eb59226
Joshua Ortiz Rosales <75183235+JoshuaOrtizR@users.noreply.github.com>
2024-11-23 13:20:31 -0600

    Add files via upload

---
+++ Python/Practices/Practice8.py
@@ -0,0 +1,19 @@
+#Tuesday, October 11, 2024
+
+#Strings
+simpleString = 'This is a Practice'
+doubleString = "This is a Practice"
+
+text = """This is only a Practice"""
+
+print(simpleString)
+print(doubleString)
+print (text)
+
+print("This is a Python Practice" + " . ")
+
+print("Python" * 4 )
+
+text1 = "Today I am practicing Python"
+
+print(text)1
