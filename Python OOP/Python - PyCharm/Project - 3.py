1a01a125b3265ef4a3f1cde42ba8f58edc5c416b
Joshua Ortiz Rosales <75183235+JoshuaOrtizR@users.noreply.github.com>
2025-07-04 17:54:48 -0600

    Add files via upload

---
+++ Python OOP/Python - PyCharm/Project - 3.py
@@ -0,0 +1,23 @@
+age = int(input("Enter your age:"))
+if age >= 18:
+    print("You are signed up")
+else:
+    print("You must be 18+ to sign up!")
+#--------------------------------------
+import math
+radio = float(input("Enter the radio of the circle:"))
+circunference = 2 * math.pi * radio
+print(f"The Circunference is:{circunference:.2f}")
+
+#-------------------------------------------
+score=float(input("What is your score:"))
+if score == 7:
+    print("You have passed, Congratulations!")
+elif score <= 0:
+    print("Your score hasn't been uploaded yet!")
+elif score >= 8:
+    print("You did an outstanding job!")
+else:
+    print("You did noy meet the required score!")
+
+#-----------------------------------------------
