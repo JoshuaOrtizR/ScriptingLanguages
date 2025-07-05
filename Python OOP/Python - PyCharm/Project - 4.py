c42c96ae01f206d2863a63b5313621fc748ad740
Joshua Ortiz Rosales <75183235+JoshuaOrtizR@users.noreply.github.com>
2025-07-05 17:05:26 -0600

    Add files via upload

---
+++ Python OOP/Python - PyCharm/Project - 4.py
@@ -0,0 +1,36 @@
+#Python Calculator
+operator = input("Enter an operator(+ - * /):")
+num0 = float(input("First number:"))
+num1 = float(input("Second number:"))
+
+if operator == "+":
+    result = num0 + num1
+    print(f"Your Result is {result}")
+elif operator == "-":
+    result = num0 - num1
+    print(f"{num0}{num1}")
+elif operator =="*":
+    result = num0 * num1
+    print(result)
+elif operator =="/":
+    result = num0 / num1
+    print(result)
+else:
+    print(f"Value Declined {operator}")
+
+#-------------------------------------------
+#python weight converter
+options = input("1: Convert Pounds (LB) to Kilograms (KG) \n2: Convert Kilograms (KG) to Pounds (LB) \nSelect an option (1 or 2):")
+
+if options == "1":
+    lb = float(input("Insert your weight in Pounds (LB): "))
+    convertion1 = lb * 0.453592
+    print(f"{convertion1:.2f} KG")
+
+elif options == "2":
+    kg = float(input("Insert your weight in Kilograms (KG):"))
+    convertion2 = kg * 2.20462
+    print(f"{round(convertion2, 2)} LB")
+else:
+    print(f"Value has been declined:{options} Try again!")
+
