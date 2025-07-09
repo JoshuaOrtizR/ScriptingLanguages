98ddbf9aeebdb585749497998233ea56e6500061
Joshua Ortiz Rosales <75183235+JoshuaOrtizR@users.noreply.github.com>
2025-07-09 00:57:50 -0600

    Update Project - 5.py

--- Python OOP/Python - PyCharm/Project - 5.py
+++ Python OOP/Python - PyCharm/Project - 5.py
@@ -1,5 +1,5 @@
 # Nested Ternary Operator
-gender = input("Insert M for (Masculine) or F (Femenine): ")
+gender = input("Insert M for (Masculine) or F (Feminine): ")
 gender = "Masculine" if gender.upper() == "M" else "Femenine" if gender.upper() == "F" else "Not valig Argument"
 print(gender)
 
@@ -16,3 +16,20 @@ grade = "A" if grade >= 90 else \
        ("B" if grade >= 80 else \
          ("C" if grade >=70 else "F"))
 print(grade)
+
+#----------------------------------------------
+#List
+list = ["I", "am", "a", "Cybersecurity Engineer", 5, [1,2,3,4]]
+print(list)
+print(type(list))
+#Tuple
+tuple = ("I","am", "a", "Cybersecurity Engineer")
+print(tuple)
+
+#Dictionary
+dict = {
+"I am a ": "Cybersecurity Engineer",
+    "Objedt Oriented": "Programming",
+    "Vulnerability": "Management"
+}
+print(dict)
