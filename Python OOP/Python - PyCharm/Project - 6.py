5b89c7f1caa9a44e08cfbd136da0c142500e7563
Joshua Ortiz Rosales <75183235+JoshuaOrtizR@users.noreply.github.com>
2025-07-09 23:16:06 -0600

    Add files via upload

---
+++ Python OOP/Python - PyCharm/Project - 6.py
@@ -0,0 +1,20 @@
+#Indexing - Slicing - Stride
+lista= ["My", "Name", "is", "Skrillex"]
+tupla = ("My", "Name", "is", "Skrillex")
+dicciona = {
+    "My": "Nombre",
+    "is": "Skrillex"
+}
+
+#indexing
+print(lista[0])
+print(dicciona["My"])
+
+#Slicing
+lista [1:3]
+print(lista)
+
+#Stride
+print (lista [2:])
+dicciona ["My"] = "You are"
+print(dicciona)
